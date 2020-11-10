24537796367ea68b6c845885ecf54edbc79f64dc
AWieczarek <adamwie12@gmail.com>
2020-11-10 14:23:03 +0100

    Add color selection

--- src/main.py
+++ src/main.py
@@ -1,8 +1,31 @@
-# CalkaCompany+
+import cv2 as cv
+import numpy as np
+cap = cv.VideoCapture(0)
+while(1):
+    # Take each frame
+    _, frame = cap.read()
+    # Convert BGR to HSV
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    # define range of blue color in HSV
+    lower_blue = np.array([100,150,0],np.uint8)
+    upper_blue = np.array([120,255,255],np.uint8)
 
-def main():
-    print("Hello World!")
+    lower_red = np.array([0,150,0],np.uint8)
+    upper_red = np.array([10,255,255],np.uint8)
+
+    # Threshold the HSV image to get only blue colors
+    mask_blue = cv.inRange(hsv, lower_blue, upper_blue)
+    mask_red = cv.inRange(hsv, lower_red, upper_red)
+
+    # Bitwise-AND mask and original image
+
+    res_blue = cv.bitwise_and(frame,frame, mask= mask_blue)
+    res_red = cv.bitwise_and(frame,frame, mask= mask_red)
+    cv.imshow('frame',frame)
+    cv.imshow('mask',mask_blue)
+    cv.imshow('res',res_blue)
 
 
-if __name__ == "__main__":
-    main()
+    if cv.waitKey(5) & 0xFF == 27:
+        break
+cv.destroyAllWindows()
